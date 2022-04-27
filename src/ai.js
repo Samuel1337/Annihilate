71b1ba5bf3a57aae3748f7313641673eeab2c5f2
bphan002 <appacademy@Mini-32.local>
2022-04-27 10:16:36 -0700

    create AI

--- src/ai.js
+++ src/ai.js
@@ -4,12 +4,74 @@ export class Ai {
         this.cap = 30*level;
         this.rate = 1;
         this.game = game;
+        this.potentialPreys = [];
+        this.potentialPredators = [];
+    }
+    
+    decideAttack() {
+        const die = this.rollDie();
 
-        this.gamePlanets = 
-        this.myPlanets = [];
+
+        this.watchForPotentialPreys();
+        this.watchForPotentialPredators();
+    }
+    
+    watchForPotentialPreys() {
+        const myStrongest = this.myStrongest()
+
+        this.game.playerPlanets.forEach(planet => {
+            if (planet.population < myStrongest) {
+                this.potentialPreys.push(planet);
+            }
+        });
+        this.potentialPreys.forEach(planet => {
+            if (planet.population > myStrongest) {
+                this.potentialPreys = this.destroy(this.potentialPreys, planet);
+            }
+        });
+    }
+    
+    watchForPotentialPredators() {
+        this.game.playerPlanets.forEach(planet => {
+            if (planet.population > this.myStrongest()) {
+                this.potentialPreys.push(planet);
+            }
+        });
+        this.potentialPredators.forEach(planet => {
+            if (planet.population > myStrongest) {
+                this.potentialPredators = this.destroy(this.potentialPredators, planet);
+            }
+        });
+    }
+    
+    myStrongest() {
+        let myStrongestPlanet = this.game.aiPlanets[0];
+        this.game.aiPlanets.forEach(planet => {
+            if (planet.population > myStrongestPlanet) {
+                myStrongestPlanet = planet;
+            }
+        });
+        return myStrongestPlanet;
+    }
+    
+    myWeakest() {
+        let myWeakestPlanet = this.game.aiPlanets[0];
+        this.game.aiPlanets.forEach(planet => {
+            if (planet.population < myWeakestPlanet) {
+                myWeakestPlanet = planet;
+            }
+        });
+        return myWeakestPlanet;
     }
 
-    watchForWeakPlanet() {
-        
+    rollDie() {
+        return Math.floor(Math.random()*6);
+    }
+
+    destroy(arr, target) {
+        const idx = arr.indexOf(target);
+        arr[idx] = arr[0];
+        arr.shift();
+        return arr;
     }
 }
